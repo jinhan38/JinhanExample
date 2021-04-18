a19c417c8d53e1e1254860093bc7e436a35ac3bf
jinhan <jinhan38@naver.com>
2021-04-18 21:33:30 +0900

    공휴일, 주말 날짜 구해오기

--- app/src/main/java/com/jinhanexample/viewPager/flexibleHeight/FlexibleViewPagerHeightActivity.kt
+++ app/src/main/java/com/jinhanexample/viewPager/flexibleHeight/FlexibleViewPagerHeightActivity.kt
@@ -2,10 +2,13 @@ package com.jinhanexample.viewPager.flexibleHeight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.viewpager.widget.ViewPager
 import com.jinhanexample.R
 import kotlinx.android.synthetic.main.activity_flexible_view_pager_height.*
 
 class FlexibleViewPagerHeightActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flexible_view_pager_height)
@@ -17,12 +20,34 @@ class FlexibleViewPagerHeightActivity : AppCompatActivity() {
 
         //offscreenPageLimit 속성을 주지 않으면 스크롤 했을 때
         //viewpager의 높이가 각 fragment들의 높이로 변합니다.
-        flexibleViewPager.offscreenPageLimit = 3
+        flexibleViewPager.offscreenPageLimit = 0
 
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         viewPagerAdapter.addItem(first)
         viewPagerAdapter.addItem(second)
         viewPagerAdapter.addItem(third)
         flexibleViewPager.adapter = viewPagerAdapter
+
+        flexibleViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int,
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                Log.d(TAG, "onPageSelected height : ${flexibleViewPager.getChildAt(position).height}")
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+        })
+    }
+
+    companion object {
+        private const val TAG = "FlexibleViewPagerHeight"
     }
 }
